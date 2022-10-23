9b35a47498fe03a99b85c2f7457f4f128db2bfea
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-10-23 11:35:05 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
+    # Dog with names #name= writes the name of the dog to an instance variable @name
+    def name=(dog_name)
+        @name = dog_name
+    end
+    def name
+        @name
+    end
+    # Dog with breeds #breed= writes the breed of the dog to an instance variable @breed
+    def breed=(dog_breed)
+        @breed = dog_breed
+    end
+    # Dog with breeds #breed reads the breed of the dog from an instance variable @breed
+    def breed
+        @breed
+    end
+
+
+
 
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,19 @@
 class Person
+    # Person with names #name= writes the name of the person to an instance variable @name
+    def name=(person_name)
+        @name = person_name
+    end
+    # Person with names #name reads the name of the person from an instance variable @name
+    def name
+        @name
+    end
+    # Person with jobs #job= writes the job of the person to an instance variable @job
+    def job=(person_job)
+        @job = person_job
+    end
+    # Person with jobs #job reads the job of the person from an instance variable @job
+    def job
+        @job
+    end
 
 end
